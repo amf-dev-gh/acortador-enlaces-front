1af8d1333615c10a64252c9bc1f17f09702ae3ee
Andres Fernandez <andres94.dev@gmail.com>
2025-05-30 15:57:56 +0200

    agregando componente Home con estructura y lógica para crear, actualizar y expandir enlaces

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { CreateLinkComponent } from "../create-link/create-link.component";
+import { UpdateLinkComponent } from "../update-link/update-link.component";
+import { ExpandLinkComponent } from "../expand-link/expand-link.component";
+
+export type forms = 'create' | 'update' | 'expand';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CreateLinkComponent, UpdateLinkComponent, ExpandLinkComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  protected showform: forms = 'create';
+
+  changeForm(form: forms) {
+    this.showform = form;
+  }
 
 }
