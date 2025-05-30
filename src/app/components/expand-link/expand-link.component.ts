9abc962097d774d0242ac57353f1c782f4afd22d
Andres Fernandez <andres94.dev@gmail.com>
2025-05-30 15:01:09 +0200

    agregando componentes About, CreateLink, ExpandLink, Home, Redirect y UpdateLink con sus respectivas plantillas base

---
+++ src/app/components/expand-link/expand-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expand-link',
+  imports: [],
+  templateUrl: './expand-link.component.html'
+})
+export class ExpandLinkComponent {
+
+}
---
+++ src/app/components/update-link/update-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-link',
+  imports: [],
+  templateUrl: './update-link.component.html'
+})
+export class UpdateLinkComponent {
+
+}
