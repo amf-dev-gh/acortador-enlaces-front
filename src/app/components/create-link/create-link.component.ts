e48ea7e59b22f356e18cf06d2c1056120b422540
Andres Fernandez <andres94.dev@gmail.com>
2025-05-31 11:39:09 +0200

    agregando lógica y estructura al componente CreateLink para manejar la creación de enlaces y validaciones

--- src/app/components/create-link/create-link.component.ts
+++ src/app/components/create-link/create-link.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { urlValidator } from '../../validators/url-validator';
+import { ApiService } from '../../services/api.service';
+import { SpinnerComponent } from "../../utils/spinner/spinner.component";
 
 @Component({
   selector: 'app-create-link',
-  imports: [],
+  imports: [ReactiveFormsModule, SpinnerComponent],
   templateUrl: './create-link.component.html'
 })
 export class CreateLinkComponent {
+
+  private fb: FormBuilder = inject(FormBuilder);
+  protected createForm: FormGroup = this.fb.group({
+    url: ['', [Validators.required, Validators.minLength(5), urlValidator]]
+  });
+
+  private apiService = inject(ApiService);
+
+  protected generatedId: string = '1a2b3c';
+  protected errorMessage: string = 'La URL ingresada ya existe en la base de datos';
+
+  createId() {
+    console.log(this.createForm.get('url')?.value)
+  }
 
 }
