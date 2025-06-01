c03824f8b23d7e04450bbdb43e3050b6dff43983
Andres Fernandez <andres94.dev@gmail.com>
2025-06-01 20:04:50 +0200

    agregando clases y estilos a los botones en el componente Home para mejorar la interfaz de usuario

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CreateLinkComponent } from "../create-link/create-link.component";
 import { UpdateLinkComponent } from "../update-link/update-link.component";
 import { ExpandLinkComponent } from "../expand-link/expand-link.component";
+import { NgClass } from '@angular/common';
 
 export type forms = 'create' | 'update' | 'expand';
 
 @Component({
   selector: 'app-home',
-  imports: [CreateLinkComponent, UpdateLinkComponent, ExpandLinkComponent],
+  imports: [CreateLinkComponent, UpdateLinkComponent, ExpandLinkComponent, NgClass],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
