3a3d22b4e1e0629c425cc38f268a1136775f02b8
Andres Fernandez <andres94.dev@gmail.com>
2025-06-02 18:03:09 +0200

    eliminando el uso de withFetch en la configuración del cliente HTTP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,5 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient(withFetch())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()), provideHttpClient()]
 };
