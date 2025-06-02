b19009ac15b5d8c2c31193d1183aa74ee385ce19
Andres Fernandez <andres94.dev@gmail.com>
2025-06-02 11:26:32 +0200

    implementando redirect y logica con manejo de errores en componente redirect

--- src/app/components/redirect/redirect.component.ts
+++ src/app/components/redirect/redirect.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-redirect',
   imports: [],
   templateUrl: './redirect.component.html'
 })
-export class RedirectComponent {
+export class RedirectComponent implements OnInit {
+
+  protected linkId = input<string>('');
+  private apiService = inject(ApiService);
+
+  protected notFoundError: boolean = false;
+  protected serverError: boolean = false;
+  protected invalidId: boolean = false;
+
+  protected fetchedUrl: string = '';
+
+  ngOnInit(): void {
+    const id = this.linkId();
+
+    if (this.isValidId(id)) {
+      this.getUrl(id);
+    } else {
+      this.invalidId = true;
+    }
+  }
+
+  private isValidId(id: string) {
+    return id.length === 6 && /^[a-zA-Z0-9]+$/.test(id);
+  }
+
+  private getUrl(id: string) {
+    this.apiService.getUrlById(id).subscribe({
+      next: data => {
+        this.fetchedUrl = data.url;
+        window.location.href = data.url;
+      },
+      error: error => {
+        if(error.status === 404){
+          this.notFoundError = true;
+        } else {
+          this.serverError = true;
+        }
+      }
+    });
+  }
 
 }
